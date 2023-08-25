7f0ce67157e33366c3f79cef642daa752360f3f3
22089 <143159233+22089@users.noreply.github.com>
2023-08-25 15:55:53 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,10 +16,23 @@ input.onGesture(Gesture.LogoDown, function () {
 })
 let y = 0
 let x = 0
+basic.showString("THIS IS GOAL!")
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.clearScreen()
 x = 0
 y = 0
 let target_y = 0
 led.plot(x, y)
 basic.forever(function () {
     led.plot(x, y)
+    if (x == 4 && y == 2) {
+        basic.showString("CLEAR!")
+    }
 })
